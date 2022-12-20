c44790c82fe7f70e2a03c2813c056479b8fbfb20
erfan taraghi <taraghikonerfan@gmail.com>
2022-12-20 22:14:39 +0330

    Header_Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import phoneHeader from "../../images/picDesign.webp"
+import './Header.css'
 const Header = () => {
-  return <div>
-    هدر سایت
-  </div>;
+  return (
+    <section id="header">
+      <div className="container header">
+        <div className="header-right">
+          <h1>
+            <span>طراحی ساختگی سادگی  </span>
+            <span>استایل مناسب </span>
+            <span>  ریسپانسیو شده </span>
+          </h1>
+          <small>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</small>
+          <div className="header-btn">
+          <a href="" className="btn btn-dark">بیاین شروع کنیم</a>
+            <a href="" className="btn btn-light">چجوری کار میکنیم</a>
+          </div>
+        </div>
+        <div className="header-left">
+          <img src={phoneHeader} alt="" />
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Header;
